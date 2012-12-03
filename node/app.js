ac6fb9bc62a9673e24ac6a01f7be0c8eb641b438
Jim R <jim@jim-ThinkPad-W520.(none)>
2012-12-03 13:30:23 -0500

    basic express application working

--- node/app.js
+++ node/app.js
@@ -1,32 +1,45 @@
 var express = require('express');
 var routes  = require('./routes');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 
 // Configuration
+
+//register ejs as .html
+app.engine('.html', require('ejs').__express);
+
+//ejs directory
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'html');
 
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
+    app.use(app.router);
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(app.router);
+
     app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+    app.set('db uri', 'localhost/dev');
+})
 
 app.configure('production', function(){
-    app.use(express.errorHandler());
-});
+    app.set('db uri', 'n.n.n.n/prod');
+})
 
 // Routes
-app.get('/block', function (req, res) {
-    res.redirect('/block.html');
+app.get('/redirect', function (req, res) {
+    res.redirect('/sRedirect.html');
 });
 
+
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode",
-    app.address().port, app.settings.env);
+console.log("Express server listening on port 3000");
+
+
+//console.log("Express server listening on port %d in %s mode",
+//    app.address().port, app.settings.env);
