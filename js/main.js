277ccab1b9dce1bc202f577eb7ab48c8df671617
Warren Alpert <WarrenAlpert@gmail.com>
2012-12-04 23:37:27 -0500

    preliminary dashboard upgrade work

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ function btnLogout()
 {
     window.location="login.html";
 }
+
+function loadDash()
+{
+    // No username stored from login, so return to login
+    if (!localStorage.username) {
+        window.location = "login.html";
+    }
+    $("#username").html(localStorage.username);
+    
+    if (!localStorage.roles) {
+        localStorage.roles = "";
+    }
+    
+    var roles = localStorage.roles.split(",");
+    for(var i in roles) {
+        alert(roles[i]);
+    }
+}
