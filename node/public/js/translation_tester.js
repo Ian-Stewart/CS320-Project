d86a61e6654987e4ddc0db8d7e863bc2d9b8a9c9
Nikita Tkachov <ntkachov@gmail.com>
2012-12-13 01:27:39 -0500

    added test to translation

--- node/public/js/translation_tester.js
+++ node/public/js/translation_tester.js
@@ -12,3 +12,37 @@
 
     console.log("getApplicationForms Success: " + getApplicationForms("jfrancis", 12345)); //12345 is the only working appID
     console.log("getApplicationForms Error: " + getApplicationForms("efrancis", 54321)); //not appID 12345 so it will return with an error
+
+
+    function callAjax(stuff0, stuff1, stuff2, stuff3, stuff4){
+        $.ajax({
+            type: 'get',
+            url: stuff0,
+            data: {'token': stuff1,
+            'applicationID': stuff4,
+            'formID': stuff2, 
+            'newStatus': stuff3},
+            dataType: "json",
+            success: function(data) {
+                console.log(data);
+            }
+        });
+    }
+
+    function saveForm(id, code){
+        callAjax("/saveForm",id,code);
+    }
+
+    function acceptApplication(id,code,code2){
+        callAjax("/acceptApplication",id,code,undefined,code2);
+    }
+
+    function rejectApplication(id, code, string){
+        callAjax("/rejectApplication",id, code, string);
+    }
+    function submitApplication(id, code){
+        callAjax("/submitApplication",id, code);
+    }
+    function getApplicationForms(id, code){
+        callAjax("/getApplicationForms",id, code);
+    }
